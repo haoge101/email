f884ae8f48beb2a2a9b2cb2c012f01eb879796c4
haoge101 <66171250+haoge101@users.noreply.github.com>
2022-12-26 16:56:57 -0300

    Update xiaoshuo.py

--- xiaoshuo.py
+++ xiaoshuo.py
@@ -217,7 +217,7 @@ if fileExist:
 
 # 登录并发送邮件
 try:
-    server = smtplib.SMTP('smtp.263.net')  # 163邮箱服务器地址，端口默认为25
+    server = smtplib.SMTP('smtp-mail.outlook.com',587)  # 163邮箱服务器地址，端口默认为25
     server.login(fromEmailAddr, password)
     server.sendmail(fromEmailAddr, toEmailAddrs, message.as_string())
     print('success')
